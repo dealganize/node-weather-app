1f3836248d13faa0e2af23d8931940d29c6dca92
dealganize <johnrogers265@gmail.com>
2020-06-02 19:32:14 +0200

    intial commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,28 @@
+const request = require('request');
+
+const forecast = (latitude, longitude, callback) => {
+  const url =
+    'http://api.weatherstack.com/current?access_key=6e6de03f1ae0c714308901103f9bd3be&query=' +
+    latitude +
+    ',' +
+    longitude;
+
+  request({ url, json: true }, (error, { body }) => {
+    if (error) {
+      callback('Unable to connect to weather service!', undefined);
+    } else if (body.error) {
+      callback('Unable to find location', undefined);
+    } else {
+      callback(
+        undefined,
+        ' It is currently ' +
+          body.current.temperature +
+          ' degrees out. There is a ' +
+          body.current.precip +
+          '% chance of rain.'
+      );
+    }
+  });
+};
+
+module.exports = forecast;
